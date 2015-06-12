4112f1df5552cce2a1ed4c6b52b6258fa0bbfa9e
Nathan Davison <ndavison85@gmail.com>
2015-06-12 08:07:17 -0400

    Removed the loopbutton and saveloopsbutton views, adding the loopbutton logic into the loops view, and replacing the saveloopsbutton view with the looputilitybuttons view.

---
+++ js/views/looputilitybuttons.js
@@ -0,0 +1,43 @@
+/**
+ *
+ *
+ *
+ *
+ */
+
+define(['backbone'], function(Backbone) {
+   
+    var View = Backbone.View.extend({
+        
+        el: '#view-loop-utility-buttons',
+        
+        events: {
+            'click button#save-loops': 'saveLoops'
+        },
+        
+        addSaveLoopsButton: function() {
+            var view = this;
+            if (view.$el.find('button#save-loops').length == 0) {
+                view.getTemplate('/looper/views/saveloops.html', {}, function(res) {
+                    view.show(res, view.$el, true);
+                });
+            }
+        },
+                
+        saveLoops: function(ev) {
+            ev.preventDefault();
+            this.app.dispatcher.trigger('saving-loops');
+        },
+        
+        initialize: function() {
+            this.app.dispatcher.on('loop-added', this.addSaveLoopsButton, this);
+        },
+        
+        render: function() {}
+        
+    });
+   
+    return View;
+    
+});
+ 
