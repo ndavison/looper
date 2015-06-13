4d9e6c4d9882476bebf00bb4c109d33d7574aa6e
Nathan Davison <ndavison85@gmail.com>
2015-06-13 05:57:13 -0400

    Added the saveLoops function to the AudioFiles collection.

--- js/models/audiofiles.js
+++ js/models/audiofiles.js
@@ -25,16 +25,24 @@ define(['backbone', 'models/audiofile'], function(Backbone, AudioFile) {
         },
         
         changeVolumes: function(level) {
-            this.invoke('volume', level);
+            this.invoke('setVolume', level);
         },
         
         changePitches: function(level) {
-            this.invoke('pitch', level);
+            this.invoke('setPitch', level);
+        },
+        
+        saveLoops: function() {
+            var model = this;
+            this.forEach(function(loop) {
+                model.app.dispatcher.trigger('save-loop', {path: 'looper-audio/' + loop.get('loopId') + '.' + loop.get('fileExtension'), data: loop.get('audioData')});
+            });
         },
         
         initialize: function() {
             this.app.dispatcher.on('play-loop', this.stopAll, this);
             this.app.dispatcher.on('play-loop', this.playLoop, this);
+            this.app.dispatcher.on('save-loops', this.saveLoops, this);
             this.app.dispatcher.on('change-volume', this.changeVolumes, this);
             this.app.dispatcher.on('change-pitch', this.changePitches, this);
             this.context = new AudioContext();
