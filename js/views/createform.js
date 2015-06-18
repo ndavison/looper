dd510f9f08d2e4d29de3d335a4bb1e4144d982c8
Nathan Davison <ndavison85@gmail.com>
2015-06-18 07:05:11 -0400

    Changed the Create Form view to allow multi-file uploads with the file upload input.

--- js/views/createform.js
+++ js/views/createform.js
@@ -14,7 +14,7 @@ define(['backbone', 'jquery', 'dropboxdropins', 'models/loop'], function(Backbon
         el: '#view-createform',
         
         events: {
-            'click button#create-loop': 'createLoop',
+            'change input[name=looper-file]': 'createLoop',
             'click button#dropbox-loop': 'getFromDropbox'
         },
         
@@ -22,7 +22,7 @@ define(['backbone', 'jquery', 'dropboxdropins', 'models/loop'], function(Backbon
             var view = this;
             if (view.$el.find('button#dropbox-loop').length == 0) {
                 view.getTemplate('/looper/views/loadfromdropbox.html', {}, function(res) {
-                    view.show(res, view.$el.find('div#create-buttons'), true);
+                    view.show(res, view.$el.find('div:last-child'), true);
                 });
             }
         },
@@ -34,31 +34,42 @@ define(['backbone', 'jquery', 'dropboxdropins', 'models/loop'], function(Backbon
         createLoop: function(ev) {
             ev.preventDefault();
             var view = this;
-            var file = this.$el.find('input[name="looper-file"]');
-            var loop = new Loop({name: file[0].files[0].name});
-            loop.setAudioProperties({context: view.app.views.loops.model.context, volume: view.app.views.controls.getVolume(), pitch: view.app.views.controls.getPitch()});
-            loop.readFile(file[0].files[0], function(model) {
-                if (!model.get('fileType').match(/^(audio\/(mpeg|wav|)|video\/ogg)/)) {
-                    view.app.views.alerts.createAlert('The file must be a WAV, MP3 or OGG audio file.', 'danger');
-                    return;
-                }
-                view.app.dispatcher.trigger('loop-added', model);
-            });
+            var fileEl = this.$el.find('input[name="looper-file"]');
+            var loops = [];
+            var loop, file;
+            for (var i = 0; i < fileEl[0].files.length; i++) {
+                file = fileEl[0].files.item(i);
+                loop = new Loop({name: file.name});
+                loops.push(loop);
+                loop.setAudioProperties({context: view.app.views.loops.model.context, volume: view.app.views.controls.getVolume(), pitch: view.app.views.controls.getPitch()});
+                loop.readFile(file, function(loop) {
+                    if (!loop.get('fileType').match(/^(audio\/(mpeg|wav|)|video\/ogg)/)) {
+                        view.app.views.alerts.createAlert('The file must be a WAV, MP3 or OGG audio file.', 'danger');
+                        return;
+                    }
+                    view.app.dispatcher.trigger('loop-loaded', loop);
+                });
+            }
+            view.app.dispatcher.trigger('loops-added', loops);
         },
         
         getFromDropbox: function(ev) {
             ev.preventDefault();
             var view = this;
+            var loops = [];
+            var loop;
             Dropbox.appKey = view.attributes.dropboxDropinKey;
             Dropbox.choose({multiselect: true, linkType: 'direct', extensions: ['audio'], success: function(files) {
                 if (files.length > 0) {
                     for (var i = 0; i < files.length; i++) {
-                        var loop = new Loop({name: files[i].name, dropboxURL: files[i].link});
+                        loop = new Loop({name: files[i].name, dropboxURL: files[i].link});
+                        loops.push(loop);
                         loop.setAudioProperties({context: view.app.views.loops.model.context, volume: view.app.views.controls.getVolume(), pitch: view.app.views.controls.getPitch()});
-                        loop.readFromURL(files[i].link, function(model) {
-                            view.app.dispatcher.trigger('loop-added', model);
+                        loop.readFromURL(files[i].link, function(loop) {
+                            view.app.dispatcher.trigger('loop-loaded', loop);
                         });
                     }
+                    view.app.dispatcher.trigger('loops-added', loops);
                 }
             }});
         },
