34f97d98e2e7995eb2ce7ed22170c0f22f3ef0d4
Nathan Davison <ndavison85@gmail.com>
2015-07-09 18:07:41 +0800

    Added r.js build config.

---
+++ js/build.js
@@ -0,0 +1,15 @@
+({
+    baseUrl: ".",
+    name: "main",
+    paths: {
+        'jquery': '../bower_components/jquery/dist/jquery.min',
+        'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
+        'backbone': '../bower_components/backbone/backbone-min',
+        'underscore': '../bower_components/underscore/underscore-min',
+        'rsvp': '../bower_components/rsvp/rsvp.min',
+        'dropbox': '../bower_components/dropbox/lib/dropbox.min',
+        'dropboxdropins': 'https://www.dropbox.com/static/api/2/dropins',
+        'Howler': '../bower_components/howler/howler.min'
+    },
+    out: 'main-build.js'
+})
